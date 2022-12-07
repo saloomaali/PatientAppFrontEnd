45b9aba0355f66069930e32c450131414b6a75f7
Salooma <saloomaali2000@gmail.com>
2022-12-07 09:31:27 +0530

    completed

--- src/app/add-patient/add-patient.component.ts
+++ src/app/add-patient/add-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-patient',
@@ -12,10 +13,21 @@ export class AddPatientComponent {
   age = ""
   mobNo = ""
 
+constructor(private api:ApiService){}
+
   readValue=()=>{
 
     let data:any = {"ptn_Id":this.ptn_Id,"name":this.name,"age":this.age,"mobNo":this.mobNo}
+    console.log(data)
     
+    this.api.addPatient(data).subscribe(
+
+      (response:any)=>{
+
+        console.log(response)
+      }
+
+    )
   }
 
 }
